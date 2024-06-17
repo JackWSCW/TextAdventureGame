945af515469f0788fedc6eb43598a44784b697f9
JackWSCW <159732885+JackWSCW@users.noreply.github.com>
2024-06-17 12:04:56 +1200

    first

---
+++ adventuregame1.py
@@ -0,0 +1,18 @@
+if __name__ == "__main__":
+  while True:
+    
+    print("Let's start with your name: ")
+    name = input()
+    print("Good luck, " +name+ ".")
+   
+# Instructions on how to play
+
+
+
+
+# Introduce the setting
+
+
+
+
+# Options select
