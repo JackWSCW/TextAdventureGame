5e58429de5ea966b5173227ae3074d7c877cf72d
JackWSCW <159732885+JackWSCW@users.noreply.github.com>
2024-06-20 10:26:50 +1200

    start of options select scenario 1

--- adventuregame1.py
+++ adventuregame1.py
@@ -1,3 +1,5 @@
+import time
+score = 0 
 # FUNCTIONS
    
 # Instructions on how to play
@@ -13,7 +15,11 @@
 # Options select
 def option_select():
     pass
-
+print("You are escaping the plane and can only take one thing with you for a chance at survival")
+time.sleep(1.5)
+question = "What will it be?"
+a = "help a man escape who is still alive but stuck under a seat"
+b = "leave the man to die and take an emergency survial kit strapped to the wall"
 
 # MAIN CODE
 
