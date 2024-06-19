f1b331e5dec8df1573267ab3208622181e57ac09
JackH1402 <159732933+JackH1402@users.noreply.github.com>
2024-06-20 10:04:20 +1200

    version 1 setting

--- adventuregame1.py
+++ adventuregame1.py
@@ -1,3 +1,5 @@
+import time
+
 # FUNCTIONS
    
 # Instructions on how to play
@@ -6,7 +8,21 @@
 
 
 # Introduce the setting
-
+def setting():
+    time.sleep(3) # wait 3 seconds
+    print("*Alarm sounds*")
+    time.sleep(3) # wait 3 seconds
+    print("*muffled dialouge comes out the speakers*")
+    time.sleep(3) # wait 3 seconds
+    print("Brace for impact - says the Pilot")
+    time.sleep(3) # wait 3 seconds
+    print(". . . ")
+    time.sleep(3) # wait 3 seconds
+    print("The plane has crashed")
+    time.sleep(2) # wait 2 seconds
+    print("your ears are ringing")
+    time.sleep(1.5) # wait 1.5 seconds
+    print("you have to get up and escape before the plane sinks")
 
 
 
@@ -15,4 +31,3 @@
 
 
 # MAIN CODE
-
