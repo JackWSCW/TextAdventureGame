4b4bfaef2ef9ac7e4d0e2164f0a8615bbe8f2740
finn8892 <159733074+finn8892@users.noreply.github.com>
2024-06-27 09:22:42 +1200

    g

---
+++ adventuregame2.py
@@ -0,0 +1,9 @@
+#instructions on how to play 
+print("how to play")
+print("You will have to make decisions with the multiple options you are given,some options will be good and help you survive on the island, other options will be bad and if you pick too many you can die. Your objective is to make it of the island alive.")
+
+#introduce the setting 
+
+#options select
+
+
